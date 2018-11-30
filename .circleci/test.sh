4d68dd5eca4d5529c4f59a80fbedc068cebb6792
louiejtaylor <louiejtaylor@gmail.com>
2018-11-30 18:27:02 -0500

    fix test, clean up test output

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -18,10 +18,13 @@ if [ `python bin/grabseqs sra -l SRP057027 | wc -l` -ne 369 ] ; then
 fi
 
 ## test metadata download
-python bin/grabseqs sra -m -l -o $HOME/tmp_test/test_metadata/ SRP057027
-if [ `cat $HOME/tmp_test/test_metadata/SRP057027.tsv | wc -l` -ne 370 ] ; then
+python bin/grabseqs sra -m -l -o $HOME/tmp_test/test_metadata/ SRP057027 > /dev/null
+if [ `cat $HOME/tmp_test/test_metadata/SRP057027.tsv | wc -l` -ne 371 ] ; then
     exit 1
 fi
 
 # test conda install
 conda install -c louiejtaylor grabseqs
+
+# cleanup
+rm -r $HOME/tmp_test
