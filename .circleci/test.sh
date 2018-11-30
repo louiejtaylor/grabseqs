348573fd35c6567c319810cc797fd0d8acc980f3
louiejtaylor <louiejtaylor@gmail.com>
2018-11-30 18:29:42 -0500

    add useful test output

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -16,13 +16,13 @@ mkdir -p $HOME/tmp_test
 if [ `python bin/grabseqs sra -l SRP057027 | wc -l` -ne 369 ] ; then
     exit 1
 fi
-
+echo "  sample listing test passed"
 ## test metadata download
 python bin/grabseqs sra -m -l -o $HOME/tmp_test/test_metadata/ SRP057027 > /dev/null
 if [ `cat $HOME/tmp_test/test_metadata/SRP057027.tsv | wc -l` -ne 371 ] ; then
     exit 1
 fi
-
+echo "  metadata test passed"
 # test conda install
 conda install -c louiejtaylor grabseqs
 
