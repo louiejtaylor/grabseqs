dd8dd557030fc02033daa47e0694ff54a6eace69
louiejtaylor <louiejtaylor@gmail.com>
2018-11-30 18:05:40 -0500

    add list test

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# re-set-up
+set -e
+
+# setup
 export PATH=${PATH}:${HOME}/miniconda3/bin
 source activate grabseqs-test
 
+# basic tests
 python bin/grabseqs -v
 python bin/grabseqs -h
+
+# more thorough tests
+mkdir -p $HOME/tmp_test
+if [ `python bin/grabseqs sra -l SRP057027 | wc -l` -ne 369 ] ; then
+    exit 1
+fi
