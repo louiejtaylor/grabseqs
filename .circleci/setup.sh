207eb181f40ae082913bc762a1b1819c4ebbe056
louiejtaylor <louiejtaylor@gmail.com>
2018-11-30 16:57:02 -0500

    better auto-install of conda

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -6,7 +6,7 @@ pwd
 
 # setup miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p
 
 # install requirements
 conda create -n grabseqs-test
