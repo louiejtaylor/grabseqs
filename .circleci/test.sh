7b5b7e2dcdcc28dd66b50b88e4779b47a19d5456
louiejtaylor <louiejtaylor@gmail.com>
2018-11-30 18:50:31 -0500

    poke NCBI less

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -10,21 +10,24 @@ source activate grabseqs-test
 python bin/grabseqs -v
 python bin/grabseqs -h
 
-# more thorough tests
+# repo grabseqs tests
 mkdir -p $HOME/tmp_test
-## test listing
-if [ `python bin/grabseqs sra -l SRP057027 | wc -l` -ne 369 ] ; then
+
+## test sample listing, metadata download
+if [ `python bin/grabseqs sra -m -l -o $HOME/tmp_test/test_metadata/ SRP057027 | wc -l` -ne 369 ]; then
     exit 1
 fi
-echo "  sample listing test passed"
+echo ":D  sample listing test passed"
+
 ## test metadata download
-python bin/grabseqs sra -m -l -o $HOME/tmp_test/test_metadata/ SRP057027 > /dev/null
 if [ `cat $HOME/tmp_test/test_metadata/SRP057027.tsv | wc -l` -ne 371 ] ; then
     exit 1
 fi
-echo "  metadata test passed"
+echo ":D  metadata test passed"
+
 # test conda install
-conda install -c louiejtaylor grabseqs
+conda install -c louiejtaylor grabseqs > /dev/null
+echo ":D  conda install test passed"
 
 # cleanup
 rm -r $HOME/tmp_test
