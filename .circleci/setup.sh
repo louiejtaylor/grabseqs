53cc5562ecae4a3267dd084d3eb1ae51456f2136
louiejtaylor <louiejtaylor@gmail.com>
2019-01-24 15:04:03 -0500

    more ci

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -7,7 +7,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p
 
 # CircleCI fix weird error
-cat deplist.txt | xargs sudo apt-get install
+cat .circleci/deplist.txt | xargs sudo apt-get install
 
 export PATH=${PATH}:${HOME}/miniconda3/bin
 
