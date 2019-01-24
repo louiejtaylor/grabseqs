8cb41e22de7eeeafc29ef0d538483ad5b8dad0d3
louiejtaylor <louiejtaylor@gmail.com>
2019-01-24 14:39:45 -0500

    fix circleci testing

--- tests/setup.sh
+++ tests/setup.sh
@@ -6,5 +6,8 @@ set -e
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p
 
+# CircleCI fix weird error
+apt-get install libxtst6
+
 export PATH=${PATH}:${HOME}/miniconda3/bin
 
