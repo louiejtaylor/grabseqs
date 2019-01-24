e8e6689726c93ebd83bb24d46aab36a32b3f9690
louiejtaylor <louiejtaylor@gmail.com>
2019-01-24 14:58:15 -0500

    ci

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -7,7 +7,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p
 
 # CircleCI fix weird error
-sudo apt-get install libxtst6 libxi6 libgconf-2-4 libnss3 libasound2 libatk-bridge2
+sudo apt-get install libxtst6 libxi6 libgconf-2-4 libnss3 libasound2 libatk-bridge2.0-0
 
 export PATH=${PATH}:${HOME}/miniconda3/bin
 
